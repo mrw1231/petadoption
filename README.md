7149e236c1a73ea38f7aee4335aae547118c4361
Michael White <mrwhite1231@gmail.com>
2022-02-07 14:54:54 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# MERN-Stack Infrastructure
+
+Clone this repo to provide the starter code for a comprehensive MERN-Stack project including token-based authentication.
+
+## Using mern-infrastructure to Create MERN-Stack Projects in the Future
+
+Here's the process to create a new MERN-Stack project that starts with the infrastructure code:
+
+1. Clone the mern-infrastructure repo.
+2. Rename the newly mern-infrastructure folder to the name of your new project.
+3. Optionally, update the `"name": "mern-infrastructure"` in package.json.
+4. Create a new repo on your personal GH account.
+5. Copy the GH repo's URL.
+6. Update the remote's URL: `git remote set-url origin <paste the copied url>`
+7. Push for the first time: `git push -u origin main`
---
+++ src/pages/App/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+// Shouldnt have to do above after React Version 17.
+// Include in each component
+import { useState } from "react";
+import { getUser} from "../../utilities/users-service"
+import { Routes, Route } from "react-router-dom";
+import AuthPage from "../AuthPage/AuthPage";
+import NewOrderPage from "../NewOrderPage/NewOrderPage";
+import OrderHistoryPage from "../OrderHistoryPage/OrderHistoryPage";
+import './App.css';
+import NavBar from "../../components/NavBar/NavBar";
+
+function App() {
+  const [user, setUser] = useState(getUser());
+  return (
+    <main className="App">
+      {
+        user ?
+        <>
+          <NavBar user={user} setUser={setUser} />
+          <Routes>
+            <Route path="/orders/new" element={<NewOrderPage />} />
+            <Route path="/orders" element={<OrderHistoryPage />} />
+          </Routes>
+        </>
+        :
+        <AuthPage setUser={setUser} />
+      }
+    </main>
+  );
+}
+
+export default App;
